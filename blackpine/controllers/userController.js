13af3816a530ee72b2d94a1511216bf4635df90c
Jordan Burger <jordanburger22@gmail.com>
2024-09-12 18:04:03 +0000

    updated routes

--- blackpine/controllers/userController.js
+++ blackpine/controllers/userController.js
@@ -2,40 +2,37 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const axios = require('axios');
 
 const signup = async (req, res, next) => {
     try {
-        const userCheck = await User.findOne({ username: req.body.username });
+        const { username, password, email } = req.body;
+
+        const userCheck = await User.findOne({ email });
         if (userCheck) {
-            res.status(400);
-            return next(new Error('That username is already taken'));
+            return res.status(400).json({ message: 'Email is already in use' });
         }
-        const newUser = new User(req.body);
-        const savedUser = await newUser.save();
-        return res.status(201).send({ user: savedUser.withoutPassword() });
+
+        const newUser = new User({ username, password, email });
+        await newUser.save();
+        const token = jwt.sign(newUser.withoutPassword(), process.env.BLACKPINE_SECRET);
+        return res.status(201).send({ token, user: newUser.withoutPassword() });
     } catch (err) {
-        res.status(500);
-        return next(err);
+        return res.status(500).json({ message: err.message });
     }
 };
 
 const login = async (req, res, next) => {
     try {
-        const user = await User.findOne({ username: req.body.username });
-        if (!user) {
-            res.status(403);
-            return next(new Error('Username or password is incorrect'));
-        }
-        const isMatch = await user.checkPassword(req.body.password);
-        if (!isMatch) {
-            res.status(403);
-            return next(new Error('Username or password is incorrect'));
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await user.checkPassword(password))) {
+            return res.status(403).json({ message: 'Invalid email or password' });
         }
         const token = jwt.sign(user.withoutPassword(), process.env.BLACKPINE_SECRET);
-        return res.status(201).send({ token, user: user.withoutPassword() });
+        return res.status(200).send({ token, user: user.withoutPassword() });
     } catch (err) {
-        res.status(500);
-        return next(err);
+        return res.status(500).json({ message: err.message });
     }
 };
 
@@ -43,40 +40,41 @@ const googleLogin = async (req, res, next) => {
     try {
         const { idToken } = req.body;
 
-        // Verify the Google ID token
         const ticket = await client.verifyIdToken({
             idToken,
             audience: process.env.GOOGLE_CLIENT_ID
         });
 
         const payload = ticket.getPayload();
-        const { email, name } = payload; // Extract name and email from the payload
+        const { sub: googleId, email, name } = payload;
 
-        // Check if the user exists or create a new user
-        let user = await User.findOne({ $or: [{ googleId: email }, { username: email }] });
-        if (!user) {
-            user = new User({
-                email,
-                name,
-                username: email, // Use email as username for simplicity
-                googleId: email, // Store Google ID for future reference
-                role: 'user' // Set role as needed
-            });
-            await user.save();
-        }
-
-        // Generate JWT token
+        let user = await User.findOrCreateByGoogleId(googleId, email, name);
         const token = jwt.sign(user.withoutPassword(), process.env.BLACKPINE_SECRET);
 
-        return res.status(201).send({ token, user: user.withoutPassword() });
+        return res.status(200).send({ token, user: user.withoutPassword() });
     } catch (err) {
-        res.status(500);
-        return next(err);
+        return res.status(500).json({ message: err.message });
     }
 };
+
+// const facebookLogin = async (req, res, next) => {
+//     try {
+//         const { accessToken } = req.body;
+//         const response = await axios.get(`https://graph.facebook.com/me?access_token=${accessToken}&fields=id,name,email`);
+//         const { id: facebookId, email, name } = response.data;
+
+//         let user = await User.findOrCreateByFacebookId(facebookId, email, name);
+//         const token = jwt.sign(user.withoutPassword(), process.env.BLACKPINE_SECRET);
+
+//         return res.status(200).send({ token, user: user.withoutPassword() });
+//     } catch (err) {
+//         return res.status(500).json({ message: err.message });
+//     }
+// };
 
 module.exports = {
     signup,
     login,
-    googleLogin
+    googleLogin,
+    facebookLogin
 };
